1d368eb2fd0c49758fd66480438d744f21dcf38a
Alex-zReeZ <alexbeck220@gmail.com>
2024-06-10 16:06:23 +0200

    refactor: removed badge component

--- src/app/ui/DashBoard.tsx
+++ src/app/ui/DashBoard.tsx
@@ -3,10 +3,7 @@ import {
   Book,
   Bot,
   Code2,
-  CornerDownLeft,
   LifeBuoy,
-  Mic,
-  Paperclip,
   Rabbit,
   Settings,
   Settings2,
@@ -17,7 +14,6 @@ import {
   Turtle,
 } from "lucide-react";
 
-import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import {
   Drawer,
@@ -423,51 +419,12 @@ export function Dashboard() {
             </form>
           </div>
           <div className="relative flex h-full min-h-[50vh] flex-col rounded-xl bg-muted/50 p-4 lg:col-span-2">
-            <Badge variant="outline" className="absolute right-3 top-3">
-              Output
-            </Badge>
-            <div className="flex-1" />
-            <form
-              className="relative overflow-hidden rounded-lg border bg-background focus-within:ring-1 focus-within:ring-ring"
-              x-chunk="dashboard-03-chunk-1"
-            >
-              <Label htmlFor="message" className="sr-only">
-                Message
-              </Label>
-              <Textarea
-                id="message"
-                placeholder="Type your message here..."
-                className="min-h-12 resize-none border-0 p-3 shadow-none focus-visible:ring-0"
-              />
-              <div className="flex items-center p-3 pt-0">
-                <TooltipProvider>
-                  <Tooltip>
-                    <TooltipTrigger asChild>
-                      <Button variant="ghost" size="icon">
-                        <Paperclip className="size-4" />
-                        <span className="sr-only">Attach file</span>
-                      </Button>
-                    </TooltipTrigger>
-                    <TooltipContent side="top">Attach File</TooltipContent>
-                  </Tooltip>
-                </TooltipProvider>
-                <TooltipProvider>
-                  <Tooltip>
-                    <TooltipTrigger asChild>
-                      <Button variant="ghost" size="icon">
-                        <Mic className="size-4" />
-                        <span className="sr-only">Use Microphone</span>
-                      </Button>
-                    </TooltipTrigger>
-                    <TooltipContent side="top">Use Microphone</TooltipContent>
-                  </Tooltip>
-                </TooltipProvider>
-                <Button type="submit" size="sm" className="ml-auto gap-1.5">
-                  Send Message
-                  <CornerDownLeft className="size-3.5" />
-                </Button>
-              </div>
-            </form>
+            <div className="text-center h-1/2 pt-32">
+              <h1>First Graph centered in de middle (temperature)</h1>
+            </div>
+            <div className="text-center h-1/2 pt-32">
+              <h2>Second Graph centered in the middle (humidity=</h2>
+            </div>
           </div>
         </main>
       </div>
