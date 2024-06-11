f2a7c872b1fcecf29e739ef0818fdff76fa519f4
Alex-zReeZ <alexbeck220@gmail.com>
2024-06-11 14:16:04 +0200

    refactor: removed everything to redo again by adding nav bar (not responsive) and installed components

--- src/app/ui/DashBoard.tsx
+++ src/app/ui/DashBoard.tsx
@@ -1,159 +1,99 @@
 "use client";
 
 import * as React from "react";
-import { format } from "date-fns";
+
+// import for aside component
+import Link from "next/link"
 import {
-  Calendar as CalendarIcon,
+  Bell,
+  CircleUser,
   Home,
   LineChart,
+  Menu,
   Package,
   Package2,
+  Search,
   ShoppingCart,
-  Users
-} from "lucide-react";
+  Users,
+} from "lucide-react"
 
-import { cn } from "@/lib/utils";
-import { Calendar } from "@/components/ui/calendar";
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
 import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-
-import { Button } from "@/components/ui/button";
-
-import { Label } from "@/components/ui/label";
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Input } from "@/components/ui/input"
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 
-import Link from "next/link";
 
 export function Dashboard() {
-  const [date, setDate] = React.useState<Date | undefined>(new Date());
 
   return (
-    <div className="grid h-screen w-full pl-[56px]">
-      <aside className="inset-y fixed  left-0 z-20 flex h-full flex-col border-r">
-        <nav className="grid gap-1 p-2">
-          <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-            <div className="hidden border-r bg-muted/40 md:block">
-              <div className="flex h-full max-h-screen flex-col gap-2">
-                <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-                  <Link href="/" className="flex items-center gap-2 font-semibold">
-                    <Package2 className="h-6 w-6" />
-                    <span className="">Climate Guardian</span>
-                  </Link>
-                </div>
-                <div className="flex-1">
-                  <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-                    <Link
-                      href="#"
-                      className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-                    >
-                      <Home className="h-4 w-4" />
-                      Dashboard
-                    </Link>
-                    <Link
-                      href="#"
-                      className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-                    >
-                      <ShoppingCart className="h-4 w-4" />
-                      Orders
-                    </Link>
-                    <Link
-                      href="#"
-                      className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
-                    >
-                      <Package className="h-4 w-4" />
-                      Products{" "}
-                    </Link>
-                    <Link
-                      href="#"
-                      className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-                    >
-                      <Users className="h-4 w-4" />
-                      Customers
-                    </Link>
-                    <Link
-                      href="#"
-                      className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-                    >
-                      <LineChart className="h-4 w-4" />
-                      Analytics
-                    </Link>
-                  </nav>
-                </div>
-              </div>
-            </div>
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <div className="hidden border-r bg-muted/40 md:block">
+        <div className="flex h-full max-h-screen flex-col gap-2">
+          <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+            <Link href="/" className="flex items-center gap-2 font-semibold">
+              <Package2 className="h-6 w-6" />
+              <span className="">Acme Inc</span>
+            </Link>
           </div>
-        </nav>
-      </aside>
-        <main className="grid flex-1 gap-4 p-4 ml-60 md:grid-cols-2 lg:grid-cols-3">
-          <div
-            className="relative hidden flex-col items-start gap-8 md:flex"
-            x-chunk="dashboard-03-chunk-0"
-          >
-            <form className="grid w-full items-start gap-6">
-              <fieldset className="grid gap-6 rounded-lg border p-4">
-                <legend className="-ml-1 px-1 text-sm font-medium">
-                  Settings
-                </legend>
-                <div className="grid gap-3">
-                  <Label htmlFor="model">Date</Label>
-                  <Popover>
-                    <PopoverTrigger asChild>
-                      <Button
-                        variant={"outline"}
-                        className={cn(
-                          "w-[280px] justify-start text-left font-normal",
-                          !date && "text-muted-foreground",
-                        )}
-                      >
-                        <CalendarIcon className="mr-2 h-4 w-4" />
-                        {date ? format(date, "PPP") : <span>Pick a date</span>}
-                      </Button>
-                    </PopoverTrigger>
-                    <PopoverContent className="w-auto p-0">
-                      <Calendar
-                        mode="single"
-                        selected={date}
-                        onSelect={setDate}
-                        initialFocus
-                      />
-                    </PopoverContent>
-                  </Popover>
-                </div>
-                <Label htmlFor="role">Salle</Label>
-                <Select defaultValue="system">
-                  <SelectTrigger>
-                    <SelectValue placeholder="Select a role" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="ESP 1">ESP 1</SelectItem>
-                    <SelectItem value="ESP 2">ESP 2</SelectItem>
-                    <SelectItem value="ESP 3">ESP 3</SelectItem>
-                    <SelectItem value="ESP 4">ESP 4</SelectItem>
-                    <SelectItem value="ESP 5">ESP 5</SelectItem>
-                    <SelectItem value="ESP 6">ESP 6</SelectItem>
-                  </SelectContent>
-                </Select>
-              </fieldset>
-            </form>
+          <div className="flex-1">
+            <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <Home className="h-4 w-4" />
+                Dashboard
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <ShoppingCart className="h-4 w-4" />
+                Orders
+                <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
+                  6
+                </Badge>
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
+              >
+                <Package className="h-4 w-4" />
+                Products{" "}
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <Users className="h-4 w-4" />
+                Customers
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <LineChart className="h-4 w-4" />
+                Analytics
+              </Link>
+            </nav>
           </div>
-          <div className="relative flex h-full min-h-[50vh] flex-col rounded-xl bg-muted/50 p-4 lg:col-span-2">
-            <div className="text-center h-1/2 pt-32">
-              <h1>First Graph centered in de middle (temperature)</h1>
-            </div>
-            <div className="text-center h-1/2 pt-32">
-              <h2>Second Graph centered in the middle (humidity)</h2>
-            </div>
-          </div>
-        </main>
+        </div>
       </div>
-  );
+    </div>
+);
 }
