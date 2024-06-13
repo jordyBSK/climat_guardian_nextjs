839d2b2a2f7965320e9461c95b238cd898408d36
jordybsk <dylan.bossoku@jobtrek.ch>
2024-06-13 11:06:26 +0200

    feat: remove sign out in SideBarElement.tsx

--- src/app/ui/dashboard/SideBarElement.tsx
+++ src/app/ui/dashboard/SideBarElement.tsx
@@ -3,23 +3,11 @@ import NavLinksElement from "@/app/ui/dashboard/NavLinksElement";
 
 export default function SideBarElement() {
     return (
-        <div className="flex h-full flex-col px-3 py-4 md:px-2">
+        <div className="flex h-full flex-col px-3 py-4 md:px-2 border-r">
             <p className="ml-2 mb-6">
                 Climat Guardian
             </p>
-            <div>
-
-            </div>
             <NavLinksElement/>
-            <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
-                <div className="hidden h-auto w-full grow rounded-md md:block"></div>
-                <form>
-                    <button
-                        className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-gray-100 hover:text-primary md:flex-none md:justify-start md:p-2 md:px-3">
-                        <div className="hidden md:block">Sign Out</div>
-                    </button>
-                </form>
-            </div>
         </div>
     );
 }
