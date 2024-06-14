44f35648dcc486941c7e05cd696d44bbdbd84b95
jordybsk <dylan.bossoku@jobtrek.ch>
2024-06-14 11:43:28 +0200

    feat: change esp link name

--- src/app/ui/dashboard/EspLinksElement.tsx
+++ src/app/ui/dashboard/EspLinksElement.tsx
@@ -1,37 +1,35 @@
-'use client';
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
 
-import Link from 'next/link';
-import {usePathname} from 'next/navigation';
-import clsx from 'clsx';
-
-const links = [
-    {name: 'esp1', href: '/dashboard/esp'},
-    {name: 'esp2', href: '/dashboard/esp'},
-];
+const links = [{ name: "chasseron" }, { name: "pleiades" }];
 
 export default function EspLinksElement() {
-    const pathname = usePathname();
+  const pathname = usePathname();
 
-    return (
-        <>
-            {links.map((link) => {
-                return (
-                    <Link
-                        key={link.name}
-                        href={link.href}
-                        className={clsx(
-                            'flex items-center gap-3 text-sm rounded-lg px-3 py-2 text-gray-500  transition-all hover:text-primary',
-                            {
-                                '': pathname === link.href,
-                            }
-                        )}
-                    >
-                        <p className="hidden md:block">{link.name}</p>
-                    </Link>
-                );
-            })}
-            <p className="gap-3 text-sm rounded-lg px-3 py-2 text-gray-500  transition-all hover:text-primary">+ ajouter
-                un esp</p>
-        </>
-    );
+  return (
+    <>
+      {links.map((link) => {
+        const href = `/dashboard/esp/${link.name}`;
+        return (
+          <Link
+            key={link.name}
+            href={href}
+            className={clsx(
+              "flex items-center gap-3 text-sm rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-primary",
+              {
+                "": pathname === href,
+              },
+            )}
+          >
+            <p className="hidden md:block">{link.name}</p>
+          </Link>
+        );
+      })}
+      <p className="gap-3 text-sm rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-primary">
+        + ajouter un esp
+      </p>
+    </>
+  );
 }
