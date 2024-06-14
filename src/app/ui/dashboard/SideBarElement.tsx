950233f8483e826c0c57962d87273b9452332c56
Alex-zReeZ <alexbeck220@gmail.com>
2024-06-14 09:38:22 +0200

    refactor: added museum icon and adjusted style

--- src/app/ui/dashboard/SideBarElement.tsx
+++ src/app/ui/dashboard/SideBarElement.tsx
@@ -1,11 +1,16 @@
 import NavLinksElement from "@/app/ui/dashboard/NavLinksElement";
 import EspLinksElement from "@/app/ui/dashboard/EspLinksElement";
+import { Landmark } from "lucide-react";
+import Link from "next/link";
 
 export default function SideBarElement() {
   return (
-    <div className="flex h-full flex-col px-3 py-4 md:px-2 border-r">
-      <p className="ml-2 mb-6">Climat Guardian</p>
-      <div className="border-b">
+    <div className="flex h-full flex-col px-3 py-4 md:px-2 border-r gap-y-2">
+      <Link href="/" className="flex items-center gap-2 font-semibold">
+        <Landmark className="h-6 w-6" />
+        <span className="">Musée Bolo</span>
+      </Link>
+      <div className="border-b pt-2">
         <NavLinksElement />
       </div>
       <div>
