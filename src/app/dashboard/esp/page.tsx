4edf45d0c537f5a31534f53578b7ac21a607c225
jordybsk <dylan.bossoku@jobtrek.ch>
2024-06-14 09:14:10 +0200

    feat: create dashboard/esp page

--- src/app/dashboard/esp/page.tsx
+++ src/app/dashboard/esp/page.tsx
@@ -1,11 +1,9 @@
-import {DateRangeElement} from "@/app/ui/dashboard/CalendarElement";
-import {ChartElement} from "@/app/ui/dashboard/ChartElement";
 import {PieChartElement} from "@/app/ui/dashboard/PieChartElement";
+import {ChartElement} from "@/app/ui/dashboard/ChartElement";
 
 export default function Page() {
     return (
-        <div className="flex h-full min-h-[50vh] gap-y-5 flex-col rounded-xl p-4 lg:col-span-2">
-            <DateRangeElement/>
+        <div className="flex h-full min-h-[50vh] gap-y-5 flex-col rounded-xl bg-muted/50 p-4 lg:col-span-2">
             <div className="flex"><PieChartElement color={"orange"}/>
                 <PieChartElement color={"blue"}/></div>
             <div>
