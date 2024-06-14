cbaa9de7e71e1d20b6b371058347f64bf04a03da
jordybsk <dylan.bossoku@jobtrek.ch>
2024-06-14 11:44:58 +0200

    feat: with the rooting system display room ESP name

--- src/app/dashboard/esp/[esp]/page.tsx
+++ src/app/dashboard/esp/[esp]/page.tsx
@@ -1,14 +1,19 @@
+"use client";
 import {PieChartElement} from "@/app/ui/dashboard/PieChartElement";
 import {ChartElement} from "@/app/ui/dashboard/ChartElement";
 
-export default function Page() {
+export default function Page({params}: { params: any }) {
     return (
         <div className="flex h-full min-h-[50vh] gap-y-5 flex-col rounded-xl bg-muted/50 p-4 lg:col-span-2">
-            <div className="flex"><PieChartElement color={"orange"}/>
-                <PieChartElement color={"blue"}/></div>
+            <p className="uppercase font-bold text-2xl">{params.esp}</p>
+            <div className="flex">
+                <PieChartElement color={"orange"}/>
+                <PieChartElement color={"blue"}/>
+            </div>
             <div>
                 <ChartElement/>
             </div>
         </div>
-    );
+    )
+        ;
 }
