deace56d68763cd1c235d814c3b8014e1dea0980
Alex-zReeZ <alexbeck220@gmail.com>
2024-06-14 15:06:43 +0200

    refactor: fix big white space in responsive mode

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div className=" w-full">
       <HeaderElement />
       <div className="flex h-screen flex-col md:flex-row">
-        <div className="w-full flex-none md:w-64">
+        <div className="hidden md:block w-full flex-none md:w-64">
           <SideBarElement />
         </div>
         <div className="flex justify-center w-full py-5 pl-10 pr-4 lg:pr-6">
