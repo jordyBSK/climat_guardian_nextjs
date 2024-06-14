cd4bd2feb7a93fdc4bbb4db3f16271f1bb45e93d
jordybsk <dylan.bossoku@jobtrek.ch>
2024-06-14 13:10:47 +0200

    feat: update shadcn components

--- src/app/dashboard/esp/[esp]/page.tsx
+++ src/app/dashboard/esp/[esp]/page.tsx
@@ -1,19 +1,20 @@
 "use client";
-import {PieChartElement} from "@/app/ui/dashboard/PieChartElement";
-import {ChartElement} from "@/app/ui/dashboard/ChartElement";
+import { PieChartElement } from "@/app/ui/dashboard/PieChartElement";
+import { ChartElement } from "@/app/ui/dashboard/ChartElement";
+import { DateRangeElement } from "@/app/ui/dashboard/CalendarElement";
 
-export default function Page({params}: { params: any }) {
-    return (
-        <div className="flex h-full min-h-[50vh] gap-y-5 flex-col rounded-xl bg-muted/50 p-4 lg:col-span-2">
-            <p className="uppercase font-bold text-2xl">{params.esp}</p>
-            <div className="flex">
-                <PieChartElement color={"orange"}/>
-                <PieChartElement color={"blue"}/>
-            </div>
-            <div>
-                <ChartElement/>
-            </div>
-        </div>
-    )
-        ;
+export default function Page({ params }: { params: any }) {
+  return (
+    <div className="flex h-full min-h-[50vh] gap-y-5 flex-col rounded-xl bg-muted/50 p-4 lg:col-span-2">
+      <p className="uppercase font-bold text-2xl">{params.esp}</p>
+      <DateRangeElement />
+      <div className="flex">
+        <PieChartElement color={"orange"} />
+        <PieChartElement color={"blue"} />
+      </div>
+      <div>
+        <ChartElement />
+      </div>
+    </div>
+  );
 }
