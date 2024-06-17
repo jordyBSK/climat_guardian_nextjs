3cb9d7c9a7b71af26dc0b5da29f01b71b24b1feb
Alex-zReeZ <alexbeck220@gmail.com>
2024-06-17 14:22:47 +0200

    refactor: added redirect to instantly go to login page when accessing website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import LoginElement from "@/app/ui/login/LoginElement";
-
 import { redirect } from "next/navigation";
 
 export default function Page() {
   return (
-    <div className="">
-      <LoginElement />
-    </div>
+      redirect("/login")
   );
 }
