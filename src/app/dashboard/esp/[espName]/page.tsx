3f816126efa1c2fdf5e93fb35822ed9e01bd591a
Alex-zReeZ <alexbeck220@gmail.com>
2024-06-17 14:23:24 +0200

    refactor: modified code structure and logic and reformated file

--- src/app/dashboard/esp/[espName]/page.tsx
+++ src/app/dashboard/esp/[espName]/page.tsx
@@ -1,25 +1,30 @@
 "use client";
+
+// import charts
 import { PieChartHumidity } from "@/app/ui/dashboard/PieChartHumidity";
 import { ChartElement } from "@/app/ui/dashboard/ChartElement";
+import { PieChartTemperature } from "@/app/ui/dashboard/PieChartTemperature";
+
+//import date range element
 import { DateRangeElement } from "@/app/ui/dashboard/CalendarElement";
 
-
+const tempData = [{ name: "temperature", value: 23 }];
+const humiData = [{ name: "humidity", value: 38 }];
 
 export default function Page({ params }: { params: any }) {
+
   return (
-      <div className="flex h-full min-h-[50vh] gap-y-5 flex-col rounded-xl p-4 lg:col-span-2">
+    <div className="flex h-full min-h-[50vh] gap-y-5 flex-col rounded-xl p-4 lg:col-span-2">
 
-          <p className="uppercase font-bold text-2xl text-black">{params.espName}</p>
-          <DateRangeElement/>
-          <div className="flex">
-
-              <PieChartHumidity data={20} />
-
-              <PieChartHumidity data={64}/>
-          </div>
-          <div>
-              <ChartElement/>
-          </div>
+      <p className="uppercase font-bold text-2xl text-black">{params.espName}</p>
+      <DateRangeElement />
+      <div className="flex">
+        <PieChartTemperature data={tempData} />
+        <PieChartHumidity data={humiData} />
       </div>
+      <div>
+        <ChartElement />
+      </div>
+    </div>
   );
 }
