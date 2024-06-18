2ee7c3799207c9b3645ba214bd14052919187398
jordybsk <dylan.bossoku@jobtrek.ch>
2024-06-18 09:31:00 +0200

    feat: can delete an esp

--- src/app/dashboard/plan/page.tsx
+++ src/app/dashboard/plan/page.tsx
@@ -1,12 +1,5 @@
 import Plan from "@/app/ui/plan/Plan";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import { Input } from "@/components/ui/input";
 import React from "react";
-import { Button } from "@/components/ui/button";
 
 export default function Page() {
   return (
