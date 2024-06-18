8de75d73a3ef3b8c1b7032871cc4d1cd41b0c1c6
Alex-zReeZ <alexbeck220@gmail.com>
2024-06-18 09:30:46 +0200

    style: added responsive container to graph

--- src/app/ui/dashboard/ChartElement.tsx
+++ src/app/ui/dashboard/ChartElement.tsx
@@ -8,6 +8,7 @@ import {
   Tooltip,
   Legend,
   ReferenceLine,
+  ResponsiveContainer,
 } from "recharts";
 import React from "react";
 
@@ -45,45 +46,47 @@ export function ChartElement() {
     },
   ];
   return (
-    <LineChart
-      className="border-gray-300 border-2 rounded-2xl"
-      width={800}
-      height={500}
-      data={data}
-    >
-      <XAxis
-        fontSize={16}
-        tickLine={false}
-        axisLine={false}
-        dataKey="name"
-        padding={{ left: 20, right: 50 }}
-      />
-      <YAxis
-        fontSize={16}
-        tickLine={false}
-        axisLine={false}
-        padding={{ top: 50, bottom: 10 }}
-      />
-      <ReferenceLine
-        y={50}
-        label={{ value: "Max humidity", position: "top" }}
-        stroke="red"
-      />
-      <Tooltip />
-      <Legend />
-      <Line
-        type="monotone"
-        dataKey="humidity"
-        stroke="#8884d8"
-        activeDot={{ r: 8 }}
-        strokeWidth="2px"
-      />
-      <Line
-        type="monotone"
-        dataKey="temperature"
-        strokeWidth="2px"
-        stroke="#82ca9d"
-      />
-    </LineChart>
+    <ResponsiveContainer width="100%" height="100%">
+      <LineChart
+        className="border-gray-300 border-2 rounded-2xl"
+        width={800}
+        height={500}
+        data={data}
+      >
+        <XAxis
+          fontSize={16}
+          tickLine={false}
+          axisLine={false}
+          dataKey="name"
+          padding={{ left: 20, right: 50 }}
+        />
+        <YAxis
+          fontSize={16}
+          tickLine={false}
+          axisLine={false}
+          padding={{ top: 50, bottom: 10 }}
+        />
+        <ReferenceLine
+          y={50}
+          label={{ value: "Max humidity", position: "top" }}
+          stroke="red"
+        />
+        <Tooltip />
+        <Legend />
+        <Line
+          type="monotone"
+          dataKey="humidity"
+          stroke="#8884d8"
+          activeDot={{ r: 8 }}
+          strokeWidth="2px"
+        />
+        <Line
+          type="monotone"
+          dataKey="temperature"
+          strokeWidth="2px"
+          stroke="#82ca9d"
+        />
+      </LineChart>
+    </ResponsiveContainer>
   );
 }
